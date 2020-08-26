bf7223b5c5be1b1d05f0e8d30abe447aec11b03c
Liuyufei <2635090458@qq.com>
2020-08-26 23:37:37 +0800

    three commit

---
+++ Git_1/src/cn/edu/buu/Demo2.java
@@ -0,0 +1,10 @@
+package cn.edu.buu;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+    }
+}
