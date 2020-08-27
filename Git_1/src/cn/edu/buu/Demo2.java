f22606c4251824d06d9bf6794aa50ccaab3393fd
Liuyufei <2635090458@qq.com>
2020-08-27 19:16:10 +0800

    one

--- Git_1/src/cn/edu/buu/Demo2.java
+++ Git_1/src/cn/edu/buu/Demo2.java
@@ -4,8 +4,5 @@ public class Demo2 {
 
     public static void main(String[] args) {
         System.out.println("aa");
-        System.out.println("aa");
-        System.out.println("aa");
-        System.out.println("aa");
     }
 }
